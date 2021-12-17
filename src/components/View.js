d37b338392cfc44cabec2f16c1fc04759b52ff74
Shivneel-Prasad <Shivam201020@gmail.com>
2021-12-17 14:10:10 -0800

    Updated All tests

--- src/components/View.js
+++ src/components/View.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import axiosWithAuth from '../utils/axiosWithAuth';
 
